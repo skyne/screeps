01e9d9b5003c9d52a18d5ed1a3550f3c192cbbff
Daniel Heringei <daniel.heringei@incepteam.com>
2023-07-19 19:49:58 +0200

    only debug scout

--- CreepBase.js
+++ CreepBase.js
@@ -67,19 +67,25 @@ CreepBase.moveToNewRoom = function() {
 
 	if(targetRoom) {
 		if(targetRoom != this.creep.room.name) {
-			console.log('moving to new room: ' + targetRoom)
+			if(this.remember('role') == 'CreepScout') {
+				console.log('moving to new room: ' + targetRoom)
+			}
 			var exitDir = this.creep.room.findExitTo(targetRoom);
 			var exit = this.creep.pos.findClosestByRange(exitDir);
 			this.creep.moveTo(exit);
 			return true;
 		} else {
-			console.log('arrived in new room: ' + targetRoom)
+			if(this.remember('role') == 'CreepScout') {
+				console.log('arrived in new room: ' + targetRoom)
+			}
 			this.creep.moveTo(30,30);
 			var targetRoom = this.remember('targetRoom', false);
 			var srcRoom = this.remember('srcRoom', this.creep.room.name);
 		}
 	} else {
-		console.log('no target room set');
+		if(this.remember('role') == 'CreepScout') {
+			console.log('no target room set');
+		}
 		return false;
 	}
 
